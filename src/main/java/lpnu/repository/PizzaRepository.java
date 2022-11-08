46e21171c8d3322d47d69e1a5abe56885c3939d9
fxxwol <protsivroksolana2004@gmail.com>
2022-11-08 15:36:54 +0200

    added weight in method addIngredient

--- src/main/java/lpnu/repository/PizzaRepository.java
+++ src/main/java/lpnu/repository/PizzaRepository.java
@@ -1,6 +1,7 @@
 package lpnu.repository;
 
 import lpnu.entity.Pizza;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,6 +12,8 @@ import java.util.stream.Collectors;
 
 @Repository
 public class PizzaRepository {
+    @Autowired
+    IngredientRepository ingredientRepository;
     private List<Pizza> pizzas = new ArrayList<>();
     private Long id = 0L;
 
@@ -50,18 +53,25 @@ public class PizzaRepository {
 
         return saved;
     }
+
     public Pizza addIngredient(final Long pizzaId, final Long ingredientId, final Integer portions) {
         final Pizza pizzaToChange = findById(pizzaId);
         final Map<Long, Integer> newIngredients = new HashMap<>(pizzaToChange.getIngredients());
+        int changedWeight = pizzaToChange.getWeight();
 
-        if (newIngredients.keySet().contains(ingredientId)) {
+        if (newIngredients.containsKey(ingredientId)) {
             newIngredients.entrySet().stream()
                     .filter(e -> e.getKey().equals(ingredientId))
                     .collect(Collectors.toMap(Map.Entry::getKey, v -> v.setValue(v.getValue() + portions)));
         } else {
             newIngredients.put(ingredientId, portions);
         }
+        changedWeight += newIngredients.entrySet()
+                .stream()
+                .mapToInt(e -> ingredientRepository.findById(e.getKey()).getWeight() * e.getValue()).sum();
+
         pizzaToChange.setIngredients(newIngredients);
+        pizzaToChange.setWeight(changedWeight);
 
         return pizzaToChange;
     }
